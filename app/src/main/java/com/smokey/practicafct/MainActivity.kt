0f8063d6653dc413a60dc3961003e8353cbfa5d5
Alejandro <alejandro.chat.propio@gmail.com>
2024-04-12 09:37:10 +0200

    Segunda versión de la aplicación. Navegación a Práctica 1 y 2 realizada

--- app/src/main/java/com/smokey/practicafct/MainActivity.kt
+++ app/src/main/java/com/smokey/practicafct/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.smokey.practicafct
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imArrow: ImageButton
+    private lateinit var imArrow2: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
     }
+
+    private fun initComponents() {
+        imArrow = findViewById(R.id.imArrow)
+        imArrow2 = findViewById(R.id.imArrow2)
+    }
+
+    private fun initListeners() {
+        imArrow.setOnClickListener {
+            navigateToPracticaUno()
+        }
+        imArrow2.setOnClickListener {
+            navigateToPracticaDos()
+        }
+    }
+
+    private fun navigateToPracticaUno() {
+        val intent = Intent(this, PracticaUno::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToPracticaDos() {
+        val intent = Intent(this, PracticaDos::class.java)
+        startActivity(intent)
+    }
+
 }
