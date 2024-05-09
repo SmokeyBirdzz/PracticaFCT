f1884d2b295509f8e8477b80b648b2bdc596c2da
Alejandro <alejandro.chat.propio@gmail.com>
2024-05-09 14:43:18 +0200

    Decimoquinta versión de la aplicación.Filtro con Toolbar ,volver atrás funcional y Datepickers también.

--- app/src/main/java/com/smokey/practicafct/ui/fragments/FragmentFiltradoFacturas.kt
+++ app/src/main/java/com/smokey/practicafct/ui/fragments/FragmentFiltradoFacturas.kt
@@ -1,35 +1,41 @@
 package com.smokey.practicafct.ui.fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toolbar
+import androidx.navigation.fragment.findNavController
 import com.smokey.practicafct.R
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentFiltradoFacturas.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentFiltradoFacturas : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+private lateinit var btnDesde : Button
+private lateinit var btnHasta : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val toolBar = view.findViewById<Toolbar>(R.id.toolBarTitle)
+        val cancelButton = view.findViewById<ImageButton>(R.id.imageButtonCancel)
+        btnDesde = view.findViewById(R.id.btnDesde)
+        btnHasta = view.findViewById(R.id.btnHasta)
+
+        cancelButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentFiltradoFacturas_to_fragmentListadoFacturas)
+        }
+        btnDesde.setOnClickListener { showDatePickerDesde(it) }
+        btnHasta.setOnClickListener { showDatePickerHasta(it) }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +44,33 @@ class FragmentFiltradoFacturas : Fragment() {
         return inflater.inflate(R.layout.fragment_filtrado_facturas, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentFiltradoFacturas.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentFiltradoFacturas().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun showDatePickerDesde(view: View) {
+        val cal = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, month, dayOfMonth ->
+                val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                btnDesde.text = selectedDate
+            },
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
+    fun showDatePickerHasta(view: View) {
+        val cal = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, month, dayOfMonth ->
+                val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                btnHasta.text = selectedDate
+            },
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
 }
